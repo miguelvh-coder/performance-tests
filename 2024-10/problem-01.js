f3cdf28f03341835e5542444fb142cfa183309e0
miguelvh-coder <72145218+miguelvh-coder@users.noreply.github.com>
2024-04-13 14:08:25 -0500

    solver problem-01.js
    algoritmo para resolver el problema de la lluvia planteado

--- 2024-10/problem-01.js
+++ 2024-10/problem-01.js
@@ -1,5 +1,29 @@
 function waterTrap(height) {
-  return 1;
+  let top = Math.max(...height)
+    var glub = 0
+    //revisa por niveles del suelo que puede si hay muros para contener agau
+    for(j=1;j<=top;j++){
+        var c = false
+        var acumulador = 0
+        for(i=0;i<height.length;i++){
+            if(c==true){
+
+                if(height[i]<j){
+                    acumulador++
+                }else{
+                    c = false
+                }
+
+            }
+            if(height[i]>=j){
+                c = true
+                glub += acumulador
+                acumulador = 0
+            }
+            
+        }
+    }
+    return glub
 };
 
 module.exports = { waterTrap };
