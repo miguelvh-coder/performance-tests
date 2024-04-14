7df78641cbe191dadebd1a52ed21c43e4f544d39
miguelvh-coder <72145218+miguelvh-coder@users.noreply.github.com>
2024-04-13 22:54:57 -0500

    solver problem-03.js

--- 2024-10/problem-03.js
+++ 2024-10/problem-03.js
@@ -1,3 +1,25 @@
 function maxArea(height) {
-  return 0;
+  if (height === null || height.length == 0) {
+        return 0
+    }
+    
+    let max = 0
+    let izq = 0
+    let der = height.length - 1
+
+    while (izq < der) {
+        //similar al problema de trapping rain water
+        let width = der - izq
+        let min = Math.min(height[izq], height[der])
+        let area = width * min
+        max = Math.max(max, area)
+
+        if (height[izq] < height[der]) {
+            izq++
+        } else {
+            der--
+        }
+    }
+
+    return max
 }
